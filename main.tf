fcd19c57a284b6a9913c8be4708cf64d6d13c3f0
Justin DeFrank <justin.defrank@gmail.com>
2023-09-19 12:29:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "vpc" {
 
   name                 = "education"
   cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
+  azs                  = ["us-east-2a", "us-east-2b", "us-east-2c"]
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_dns_hostnames = true
   enable_dns_support   = true
